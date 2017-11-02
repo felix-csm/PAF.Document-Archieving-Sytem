8f162b39bc85443b3ad03815d0398508c91c2a87
Amen Manibo <reneamenmanibo@gmail.com>
2017-11-02 12:53:30 +0800

    Pre-defined users added

--- PAF.DAS.Service/DAL/DBInitializer.cs
+++ PAF.DAS.Service/DAL/DBInitializer.cs
@@ -31,14 +31,28 @@ namespace PAF.DAS.Service.DAL
             if (!_context.Roles.Any(r => r.Name == "Administrator"))
             {
                 await _roleManager.CreateAsync(new IdentityRole("Administrator"));
+                //Administrator user account
                 await _userManager.CreateAsync(new ApplicationUser { UserName = "admin@application.com", Email = "admin@application.com" }, "12345Admin$");
                 await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("admin@application.com"), "Administrator");
+                //AFOS Administrator user account
+                await _userManager.CreateAsync(new ApplicationUser { UserName = "AFOSAdmin@application.com", Email = "AFOSAdmin@application.com" }, "12345Afos$");
+                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("AFOSAdmin@application.com"), "Administrator");
+                //NCOS Administrator user account
+                await _userManager.CreateAsync(new ApplicationUser { UserName = "NCOSAdmin@application.com", Email = "NCOSAdmin@application.com" }, "12345Ncos$");
+                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("NCOSAdmin@application.com"), "Administrator");
+                //PAFTSTS Administrator user account
+                await _userManager.CreateAsync(new ApplicationUser { UserName = "PAFTSTSAdmin@application.com", Email = "PAFTSTSAdmin@application.com" }, "12345Paftsts$");
+                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("PAFTSTSAdmin@application.com"), "Administrator");
             }
 
             if (!_context.Roles.Any(r => r.Name == "Guest")) {
                 await _roleManager.CreateAsync(new IdentityRole("Guest"));
-                await _userManager.CreateAsync(new ApplicationUser { UserName = "guest@application.com", Email = "guest@application.com" }, "12345Guest$");
-                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("guest@application.com"), "Guest");
+                //Researcher user account
+                await _userManager.CreateAsync(new ApplicationUser { UserName = "researcher@application.com", Email = "researcher@application.com" }, "12345Researcher$");
+                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("researcher@application.com"), "Guest");
+                //Student user account
+                await _userManager.CreateAsync(new ApplicationUser { UserName = "student@application.com", Email = "student@application.com" }, "12345Student$");
+                await _userManager.AddToRoleAsync(await _userManager.FindByNameAsync("student@application.com"), "Guest");
             }
         }
     }
