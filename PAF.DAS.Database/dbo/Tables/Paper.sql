d0a7fb6a74ee2866308256ca5ece5855c0084685
HOMEPC\felixcsm <felixcsm@gmail.com>
2017-11-12 16:48:29 +0800

    deploy updates

--- PAF.DAS.Database/dbo/Tables/Paper.sql
+++ PAF.DAS.Database/dbo/Tables/Paper.sql
@@ -10,9 +10,9 @@ index key, rounded up to the nearest power of two.
 CREATE TABLE [dbo].[Paper]
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED DEFAULT newid(), 
-    [Title] NVARCHAR(100) NULL, 
+    [Title] NVARCHAR(500) NULL, 
     [Author] NVARCHAR(100) NULL, 
     [DocumentType] INT NULL, 
     [YearSubmitted] NCHAR(4) NULL, 
-    [Remarks] NVARCHAR(300) NULL
+    [Remarks] NVARCHAR(1000) NULL
 )
