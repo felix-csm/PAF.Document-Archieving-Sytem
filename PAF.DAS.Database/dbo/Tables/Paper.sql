95081864c03200dc541fbc01fe328e1a43fea0ba
Amen Manibo <reneamenmanibo@gmail.com>
2017-11-14 15:15:13 +0800

    Paper Title to 1000 characters Truncated title at stats

--- PAF.DAS.Database/dbo/Tables/Paper.sql
+++ PAF.DAS.Database/dbo/Tables/Paper.sql
@@ -10,7 +10,7 @@ index key, rounded up to the nearest power of two.
 CREATE TABLE [dbo].[Paper]
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED DEFAULT newid(), 
-    [Title] NVARCHAR(500) NULL, 
+    [Title] NVARCHAR(1000) NULL, 
     [Author] NVARCHAR(100) NULL, 
     [DocumentType] INT NULL, 
     [YearSubmitted] NCHAR(4) NULL, 
