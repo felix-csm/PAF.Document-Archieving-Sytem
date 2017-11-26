8fa53b5530a761240c1249f3b7b973eb22f90e00
katax\rmanibo <xacden@gmail.com>
2017-11-26 12:42:00 +0800

    deploy update

--- PAF.DAS.WebAPI/Program.cs
+++ PAF.DAS.WebAPI/Program.cs
@@ -1,17 +1,26 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace PAF.DAS.WebAPI
 {
     public class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            var baseUrl = Configuration.GetSection("BaseUrl").Value;
+            BuildWebHost(args, baseUrl).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args, string baseUrl) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls(baseUrl)
                 .UseStartup<Startup>()
                 .Build();
     }
